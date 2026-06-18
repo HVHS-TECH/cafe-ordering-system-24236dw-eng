323168d23bfbf9880aa0c77b96354b8cb0ee4b26
24236dw-eng <24236dw@hvhs.school.nz>
2026-06-18 21:43:19 +0000

    Jun 18, 2026, 9:43 PM

---
+++ script.js
@@ -0,0 +1,28 @@
+<body>
+  <h1>T11-data types</h1>
+  <p>Open the console <code>(Ctrl-shift-i)</code> to see the messages.</p>
+<button onClick="start()">start </button>
+
+
+<form onsubmit="return false;">
+<label for=":formName">username:</label>
+<input type="text" id="nameField" name="formName" placeholder="username">
+<label for=":formName">user age:</label>
+<input type="number" id="ageField" name="formName" placeholder="userage">
+<label for=":formName">Pocket money:</label>
+<input type="number" id="moneyField" name="formName" placeholder="Pocket money">
+<input type="submit" onClick=getFormInput()>
+</form>
+
+  <div id="spaceForJavaScriptOutput">
+
+    The output of your script will go here.
+
+  </div>
+
+
+  <script src="../t11_data_types.js"></script>
+
+</body>
+
+</html>
