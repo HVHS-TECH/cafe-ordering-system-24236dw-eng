2c00b1392b881c4b9c2a53444646fa5ee531ffea
24236dw-eng <24236dw@hvhs.school.nz>
2026-06-22 23:35:08 +0000

    Jun 22, 2026, 11:35 PM

--- script.js
+++ script.js
@@ -12,4 +12,8 @@ function getFormInput(){
 const NAME_FIELD = document.getElementById("nameField");
 let Username = NAME_FIELD.value;
 OUTPUT.innerHTML += "<p> Hi " + Username + "</p>";
+
+const ORDER_FIELD = document.getElementById("orderField");
+let Order = ORDER_FIELD.value;
+OUTPUT.innerHTML += "<p> Your " + Order + "</p>";
 }
