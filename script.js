9f2f0d47aa8fb7b1f651835ff27720083a15a181
24236dw-eng <24236dw@hvhs.school.nz>
2026-06-24 02:33:41 +0000

    Jun 24, 2026, 2:33 AM

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ OUTPUT.innerHTML += "<h2></h2>";
 main code
 
 ****************************/
+const MONEY_FIELD = document.getElementById("moneyField");
+let UserpocketMoney = Number(MONEY_FIELD.value);
+
 function getFormInput(){
 const NAME_FIELD = document.getElementById("nameField");
 let Username = NAME_FIELD.value;
@@ -20,4 +23,16 @@ OUTPUT.innerHTML += "<p> Your order is " + Order + "</p>";
 const MONEY_FIELD = document.getElementById("moneyField");
 let Money = MONEY_FIELD.value;
 OUTPUT.innerHTML += "<p> You have " + Money + "</p>";
+}
+function calculateChange(_money, _price){
+let change = _money - _price
+return change;
+}
+    if (money >= 4){
+        OUTPUT.innerHTML += "<p>you have enough to buy a chocolate bar";
+        OUTPUT.innerHTML += "<p>you will get $"+calculateChange(money, chocolatePrice)+" change";
+    }
+    else{
+        OUTPUT.innerHTML += "<p>you do not have enough to buy a chocolate bar"
+    }
 }
